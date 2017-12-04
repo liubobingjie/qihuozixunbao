9eb9d2fe808bdd5007e19d8c605550f38f3d1051
liubobingjie <1764347272@qq.com>
2017-12-04 16:19:43 +0800

    初始化项目

---
+++ qihuobaode/qihuobaode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qihuobaode
+//
+//  Created by mc on 2017/12/4.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qihuobaode/qihuobaode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qihuobaode
+//
+//  Created by mc on 2017/12/4.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
